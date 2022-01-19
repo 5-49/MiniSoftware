942b4fb5837121218d88fbfb02051db5c2727234
YangJing <1854025@tongji.edu.cn>
2022-01-19 21:18:21 +0800

    上传小程序1.0.0版本

--- WhereLonelinessEnds/pages/movie/movie.js
+++ WhereLonelinessEnds/pages/movie/movie.js
@@ -1,66 +1,54 @@
-// pages/movie/movie.js
-Page({
+const quote = [
+  'https://s2.loli.net/2022/01/15/3SxfZKeYGpu7NoW.jpg',
+  'https://s2.loli.net/2022/01/15/M7RfeKmadE8tQjS.jpg',
+  'https://s2.loli.net/2022/01/15/chpUNPJaqIe6BGn.jpg',
+  'https://s2.loli.net/2022/01/15/uF1N5ckovYUaM9J.jpg',
+  'https://s2.loli.net/2022/01/15/WJuHqA46ixab7RG.jpg',
+  'https://s2.loli.net/2022/01/15/ESBNVszXr2F5Zmb.jpg',
+  'https://s2.loli.net/2022/01/15/2VOYTxGyzPro1lR.jpg',
+  'https://s2.loli.net/2022/01/15/VSceP2wGUInt3j4.jpg',
+  'https://s2.loli.net/2022/01/15/KgeLCVOcXQ1itTh.jpg',
+  'https://s2.loli.net/2022/01/15/h2EfDiG3b6PjevM.jpg',
+  '../images/movie/11.jpg',
+  '../images/movie/12.jpg',
+  '../images/movie/13.jpg',
+  '../images/movie/14.jpg',
+  '../images/movie/15.jpg',
+  '../images/movie/16.jpg',
+  '../images/movie/17.jpg',
+  '../images/movie/18.jpg',
+  '../images/movie/19.jpg',
+  '../images/movie/20.jpg'
+]
 
+Page({
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+    return {
+      title: 'text',
+      path: 'page/component/pages/book/book'
+    }
+  },
   /**
    * 页面的初始数据
    */
   data: {
-
+    text: 'https://s2.loli.net/2022/01/15/chpUNPJaqIe6BGn.jpg',
   },
+  nextQuote: ['test'],
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  change() {
+    this.nextQuote.pop()
+    this.nextQuote.push(quote[Math.floor((Math.random() * 20))])
+    this.setData({
+      text: this.nextQuote
+    })
+    setTimeout(() => {
+      this.setData({
+        scrollTop: 99999
+      })
+    }, 0)
   }
 })
--- WhereLonelinessEnds/pages/song/song.js
+++ WhereLonelinessEnds/pages/song/song.js
@@ -1,66 +1,53 @@
-// pages/song/song.js
-Page({
+const quote = [
+  '../images/song/01.jpg',
+  '../images/song/02.jpg',
+  '../images/song/03.jpg',
+  '../images/song/04.jpg',
+  '../images/song/05.jpg',
+  '../images/song/06.jpg',
+  '../images/song/07.jpg',
+  '../images/song/08.jpg',
+  '../images/song/09.jpg',
+  '../images/song/10.jpg',
+  '../images/song/11.jpg',
+  '../images/song/12.jpg',
+  '../images/song/13.jpg',
+  '../images/song/14.jpg',
+  '../images/song/15.jpg',
+  '../images/song/16.jpg',
+  '../images/song/17.jpg',
+  '../images/song/18.jpg',
+  '../images/song/19.jpg'
+]
 
+Page({
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+    return {
+      title: 'text',
+      path: 'page/component/pages/book/book'
+    }
+  },
   /**
    * 页面的初始数据
    */
   data: {
-
+    text: '../images/song/16.jpg',
   },
+  nextQuote: ['test'],
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  change() {
+    this.nextQuote.pop()
+    this.nextQuote.push(quote[Math.floor((Math.random() * 19))])
+    this.setData({
+      text: this.nextQuote
+    })
+    setTimeout(() => {
+      this.setData({
+        scrollTop: 99999
+      })
+    }, 0)
   }
 })
